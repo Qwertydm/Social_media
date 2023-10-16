a28d58e1f752a24188f0b239972d1cc8059bd387
Qwertydm <kanken596@gmail.com>
2023-10-16 20:08:48 +0500

    Initial commit

---
+++ comments/__init__.py
@@ -0,0 +1,5 @@
+from fastapi import APIRouter
+
+comment_router = APIRouter(prefix='/comments', tags=['Комментарии к публикациям'])
+
+from comments import comments_api
---
+++ profile/__init__.py
@@ -0,0 +1,6 @@
+from fastapi import APIRouter
+
+
+profile_router = APIRouter(prefix='/profile', tags=['Профиль пользователей'])
+
+from profile import profile_api
